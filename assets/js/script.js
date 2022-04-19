f64ede60649a303f22a5c889505f0f3665b91f4c
Alex Miramontes <alex.r.miramontes@gmail.com>
2022-04-19 16:02:20 -0700

    removed commented out code from script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,6 +23,8 @@ function dateCreate() {
 dateCreate();
 
 
+// conditional statements to apply appropriate text area color depending on time of day
+
 function timeTracker() {
     let currentHour = parseInt(moment().format('HH'));
 
@@ -40,84 +42,6 @@ function timeTracker() {
 }
 
 timeTracker();
-
-
-
-
-// conditional statements to apply appropriate text area color depending on time of day
-
-// if (currentTime == 9) {
-//     $('#9am-tasks').addClass("present");
-// } else if (currentTime > 9) {
-//     $('#9am-tasks').addClass("past");
-// } else {
-//     $('#9am-tasks').addClass("future");
-// }
-
-// if (currentTime == 10) {
-//     $('#10am-tasks').addClass("present");
-// } else if (currentTime > 10) {
-//     $('#10am-tasks').addClass("past");
-// } else {
-//     $('#10am-tasks').addClass("future");
-// }
-
-// if (currentTime == 11) {
-//     $('#11am-tasks').addClass("present");
-// } else if (currentTime > 11) {
-//     $('#11am-tasks').addClass("past");
-// } else {
-//     $('#11am-tasks').addClass("future");
-// }
-
-// if (currentTime == 12) {
-//     $('#12pm-tasks').addClass("present");
-// } else if (currentTime > 12) {
-//     $('#12pm-tasks').addClass("past");
-// } else {
-//     $('#12pm-tasks').addClass("future");
-// }
-
-// if (currentTime == 13) {
-//     $('#1pm-tasks').addClass("present");
-// } else if (currentTime > 13) {
-//     $('#1pm-tasks').addClass("past");
-// } else {
-//     $('#1pm-tasks').addClass("future");
-// }
-
-// if (currentTime == 14) {
-//     $('#2pm-tasks').addClass("present");
-// } else if (currentTime > 14) {
-//     $('#2pm-tasks').addClass("past");
-// } else {
-//     $('#2pm-tasks').addClass("future");
-// }
-
-// if (currentTime == 15) {
-//     $('#3pm-tasks').addClass("present");
-// } else if (currentTime > 15) {
-//     $('#3pm-tasks').addClass("past");
-// } else {
-//     $('#3pm-tasks').addClass("future");
-// }
-
-// if (currentTime == 16) {
-//     $('#4pm-tasks').addClass("present");
-// } else if (currentTime > 16) {
-//     $('#4pm-tasks').addClass("past");
-// } else {
-//     $('#4pm-tasks').addClass("future");
-// }
-
-// if (currentTime == 17) {
-//     $('#5pm-tasks').addClass("present");
-// } else if (currentTime > 17) {
-//     $('#5pm-tasks').addClass("past");
-// } else {
-//     $('#5pm-tasks').addClass("future");
-// }
-
 
 
 // click functions for the save button on each hour that commit the value of text area to local storage, along with code that allows the same information to be retrived from local storage and appended back to the text area on page reload
